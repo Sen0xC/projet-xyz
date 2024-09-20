4f6050dec2e9bf29d30fbe55ffceeffcd55169b8
Cyril Romero <cyril.romero@etu.unilim.fr>
2024-09-20 13:51:32 +0200

    ajout database

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
 
 class User extends Authenticatable
 {
@@ -43,5 +45,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Get the user's username.
+     */
+    protected function username(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => "user". str_pad((string) $this->id, 4,"0", STR_PAD_LEFT),
+        );
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,15 +9,12 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+Seed the application's database.*/
+    public function run(): void{
+
+        User::factory()->count(10)->create();
     }
+
+
 }
