3b2db96ea0acd3278ebbecdd7e09c1401f90e40b
Jimmy-legg <Jimmy.legg18@gmail.com>
2024-09-20 13:49:35 +0200

    Update User php

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class User extends Authenticatable
 {
@@ -44,4 +45,16 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    /**
+     * Summary of username
+     * @return \Illuminate\Database\Eloquent\Casts\Attribute
+     */
+    protected function username(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => "user" . str_pad($this->id, 4, '0', STR_PAD_LEFT),
+        );
+    }
+    
 }
